febf0503a5269fe62b132944407d91b23c1df50d
Syd_matinnn_01 <saiyedmatin123@gmail.com>
2026-02-22 01:53:39 +0530

    installing shadcn UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react"
+import { Button } from "./components/ui/button"   // make sure shadcn installed
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-  Tailwind Working </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Tailwind Working
+      </h1>
 
+      <button className="bg-red-500" variant="ghost">
+       Click me
+      </button>
+    </div>
   )
 }
 
-export default App  
+export default App
